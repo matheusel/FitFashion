db252c23979ed33bd4cf2dedce240bc156591c4a
Matheus El <matheuselh@gmail.com>
2022-09-30 01:10:16 -0300

    'Página principal

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ menuCelular.addEventListener('click', () => {
   menuSection.classList.toggle("on", show)
   show = !show
 });
+
+/* let showFooter = true
+
+const menuzin = document.querySelector('.menu')
+const footer = menuzin.querySelector('.footer-container')
+
+footer.addEventListener('click', () => {
+  menuzin.style.display('none', showFooter)
+  showFooter = !showFooter
+}); */
+
+
+/* function hideFooter() {
+    var clickfooter = document.querySelector(".menu")
+    var footer = clickfooter.querySelector(".footer-container")
+    clickfooter.addEventListener("click", footer)
+    footer.style.display('none', footer)
+
+}
+console.log(hideFooter) */
